dd952f361b575c25cd601727a8de607a14fb98dd
Ronn340 <ronravnr@gmail.com>
2025-09-25 21:50:46 +1000

    added toolbar and actions

--- Hellow world/ViewController.swift
+++ Hellow world/ViewController.swift
@@ -9,7 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var Label: UILabel!
+    @IBOutlet weak var message: UILabel!
     
+    @IBOutlet weak var add_dish: UIBarButtonItem!
+    @IBOutlet weak var view_dish: UIBarButtonItem!
+    @IBOutlet weak var edit_order: UIBarButtonItem!
+    @IBOutlet weak var add_order: UIBarButtonItem!
     @IBOutlet weak var Input: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,5 +25,17 @@ class ViewController: UIViewController {
         Label.text = "Hello " + Input.text!
     }
     
+    @IBAction func add_dish(_ sender: Any) {
+        message.text = add_dish.title! + " clicked"
+    }
+    @IBAction func view_dish(_ sender: Any) {
+        message.text = view_dish.title! + " clicked"
+    }
+    @IBAction func edit_order(_ sender: Any) {
+        message.text = edit_order.title! + " clicked"
+    }
+    @IBAction func add_order(_ sender: Any) {
+        message.text = add_order.title! + " clicked"
+    }
 }
 
